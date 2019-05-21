feb7a7f97a10cd1414172617c49f065333a57806
MangoYogurt-zhanxiaotong <1176861386@qq.com>
2019-05-21 13:14:25 +0800

    chunk

--- util.js
+++ util.js
@@ -1,2 +1,15 @@
-let util = {};
-// add something
+let util = {
+	chunk: function(array, size = 1) {
+		size = Math.max(size,0);
+		let length = array == null ? 0 : array.length;
+		if(!length || size < 1){
+			return [];
+		}
+		let result = []; // 存放拆分结果
+		let rl = Math.ceil(length / size); // 向上舍入
+		for(let i = 0; i < rl; i++){
+			result[i] = array.slice(size*i,size*i+size);
+		}
+		return result;
+	}
+};
